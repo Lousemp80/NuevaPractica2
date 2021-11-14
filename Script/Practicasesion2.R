3c204b8153b9de9de7071606cbccab80ca6ca6ac
Lousemp80 <lourdes.sempert@gmail.com>
2021-11-14 20:37:50 -0300

    mas practica para agregar

--- Script/Practicasesion2.R
+++ Script/Practicasesion2.R
@@ -54,5 +54,80 @@ round(x = edad_promedio, digits = 2)
 
 library(tidyverse)
 
-%>%  ## atajo para la pipa ctrl + Shift + M
+curso %>% ## atajo para la pipa CTRL + SHIFT + M
+  summarise(mean(edad))
+
+curso %>% 
+  summarise(promedio = mean(edad))
+
+curso %>% 
+  summarise(promedio = round(mean(edad),
+                             digits = 2)
+            )
+
+library(electorAr)
+
+elecciones <- show_available_elections(source = "data", viewer = T)
+
+class(elecciones)
+
+class(curso$edad)
+
+class(curso$nombre)
+
+class(curso$nivel)
+
+numeros_texto <- as.character(curso$nivel)
+
+class(numeros_texto)
+
+as.numeric(numeros_texto)
+
+dim(curso) ### dimensiones de CURSO
+
+### para poder ver la tabla de la libreria
+
+show_available_elections(source = "data", viewer = T)
+
+elecciones
+
+### para las general 
+
+eleccion <- get_election_data(district = "arg",
+                  category = "presi",
+                  round = "gral",
+                  year  = 2015, raw = TRUE)
+
+eleccion %>% 
+  get_names()
+
+### para las paso
+
+eleccion2 <- get_election_data(district = "arg",
+                              category = "presi",
+                              round = "paso",
+                              year  = 2015, raw = TRUE)
+
+eleccion2 %>% 
+  get_names()
+
+gral2015 <- get_election_data(district = "arg",
+                                          category = "presi",
+                                          round = "gral",
+                                          year  = 2015, level = "departamento") %>% 
+  get_names()
+
+## mas votos del FIT
+
+## version BASE
+
+fit <- gral2015[gral2015$nombre_lista == 'ALIANZA FRENTE DE IZQUIERDA Y DE LOS TRABAJADORES', ] # > BASE
+fit[fit$votos == max(fit$votos),]
+
+## Version TIDY
+
+gral2015 %>%  # > TIDY
+  filter(nombre_lista == 'ALIANZA FRENTE DE IZQUIERDA Y DE LOS TRABAJADORES') %>% 
+  ungroup() %>% 
+  slice_max(votos)
 
